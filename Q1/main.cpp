2a4d8f9a340dae4105ea7cb3dab574b7b03c8b81
xyk <42699475+quanta66@users.noreply.github.com>
2022-04-24 19:43:45 +0200

    Optimization

--- Q1/main.cpp
+++ Q1/main.cpp
@@ -9,7 +9,6 @@
 
 #include <iostream>
 
-
 using namespace std;
 
 int main()
@@ -17,33 +16,40 @@ int main()
 
     cout<<"The sum of all the multiples of x and y below an interval\n";
 
-    int multiplesOf_x, multiplesOf_y, multiplesOf_X, multiplesOf_Y;
+    int multiplesOf_x, multiplesOf_y, multiplesOf_X, multiplesOf_Y, multiplesOf_15;
     int interval;
     cout<<"Enter two numbers: ";
     cin>>multiplesOf_x;
     cin>>multiplesOf_y;
     cout<<"Enter any upper bound ([0, interval]): ";
     cin>>interval;
-    double sum=0;
+    int sum=0, sum1=0, sum2=0, sum15=0;
     for(int i=0; i<interval; i++)
     {
         multiplesOf_X = i*multiplesOf_x;
         multiplesOf_Y = i*multiplesOf_y;
+        multiplesOf_15 = i*15;
 
         if(multiplesOf_X <interval)
         {
             //out<< multiplesOf_X;
 
-            sum = sum + multiplesOf_X;
+            sum1 = sum1 + multiplesOf_X;
         }
         if(multiplesOf_Y <interval)
         {
             //cout<< multiplesOf_Y;
 
-            sum = sum + multiplesOf_Y;
+            sum2 = sum2 + multiplesOf_Y;
         }
 
-                 
+        if(multiplesOf_15 < interval)
+        {
+            sum15 = sum15 + multiplesOf_15;
+        }
+
+        sum = (sum1+sum2) - sum15;
+           
     }
 
     cout<<"The sum of all the multiples of "<<multiplesOf_x<<" and "<<multiplesOf_y<<" below "<< interval <<" is : "<<sum;
